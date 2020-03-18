f084d751fa0adf9f34943f6a7880dd526a559321
aishwaryaa-v <57423063+aishwaryaa-v@users.noreply.github.com>
2020-03-18 10:22:05 -0400

    Isomorphic string comparison for 2 strings

---
+++ main.py
@@ -0,0 +1,24 @@
+def compareStrings(str1, str2):
+    a = len(str1)
+    b = len(str2)
+    #length not equal then one to one not possible
+    if a != b:
+        return False
+
+    current =[]
+    for i in range(len(str1)):
+        current.append((str1[i], str2[i]))
+
+        #checking for same strings in string 1 as they should not be mapped to different items
+        #one to one matching
+
+    for i in range(len(str1)-1):
+        for j in range(len(str1) - 1):
+            if current[i][0] == current[j+1][0]:
+                if current[i][1] != current[j+1][1]:
+                    return False
+    return True
+
+
+result = compareStrings("aabab","xxyxy")
+print(result)
